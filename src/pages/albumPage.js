8a6ee69162756b33a31595f2c41a7ab3e5298657
andreasrobert <andreasrobb23@gmail.com>
2021-09-06 23:03:43 +0700

    align text

--- src/pages/albumPage.js
+++ src/pages/albumPage.js
@@ -5,12 +5,12 @@ import ViewPhoto from "../components/views/viewPhoto";
 import { Link } from "react-router-dom";
 
 function AlbumPage() {
-  const [albums, setAlbums] = useState([]); 
-  const [userData, setUserData] = useState([]); 
-  const [photos, setPhotos] = useState([]); 
-  const [isAllowed, setIsAllowed] = useState(false);  // won't fetch album if user doesn't have the right albumId in the url params
+  const [albums, setAlbums] = useState([]);
+  const [userData, setUserData] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [isAllowed, setIsAllowed] = useState(false); // won't fetch album if user doesn't have the right albumId in the url params
 
-  let { userId, albumId } = useParams();  // route-> siteName/user/:userId/album/:albumId
+  let { userId, albumId } = useParams(); // route-> siteName/user/:userId/album/:albumId
 
   useEffect(() => {
     const getAllUserAlbums = () => {
@@ -18,13 +18,13 @@ function AlbumPage() {
         .then((response) => response.json())
         .then((res) => setAlbums(res));
     };
-  
+
     const getUser = () => {
       fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
         .then((response) => response.json())
         .then((res) => setUserData(res));
     };
-  
+
     const getPhotos = () => {
       fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`)
         .then((response) => response.json())
@@ -39,12 +39,13 @@ function AlbumPage() {
     if (isAllowed) {
       getPhotos();
     }
-
   }, [userId, albumId, isAllowed]);
 
   return (
     <Flex flexDir="column" alignItems="center">
-      <Heading my="10px">u/{userData.username} Albums</Heading>
+      <Heading textAlign="center" my="10px">
+        u/{userData.username} Albums
+      </Heading>
 
       {/* List of user albums */}
       <Flex justifyContent="center" flexWrap="wrap" w="70%">
@@ -54,20 +55,20 @@ function AlbumPage() {
           }
           return (
             <Link key={album.id} to={`/user/${userData.id}/album/${album.id}`}>
-                <Text
-                  color={album.id == albumId ? "yellow" : "black"}
-                  bg={album.id == albumId ? "black" : "inherit"}
-                  _hover={{ bg: "black", color: "yellow" }}
-                  textAlign="center"
-                  border="2px solid"
-                  borderColor="borderColor"
-                  borderRadius="8px"
-                  minH="80%"
-                  px="3px"
-                  m="3px"
-                >
-                  {album.title}
-                </Text>
+              <Text
+                color={album.id == albumId ? "yellow" : "black"}
+                bg={album.id == albumId ? "black" : "inherit"}
+                _hover={{ bg: "black", color: "yellow" }}
+                textAlign="center"
+                border="2px solid"
+                borderColor="borderColor"
+                borderRadius="8px"
+                minH="80%"
+                px="3px"
+                m="3px"
+              >
+                {album.title}
+              </Text>
             </Link>
           );
         })}
